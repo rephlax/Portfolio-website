1eb6974fd1cd03ce01afe7ac677e6cb11fa9baf1
Luke Farrell <lukefarrell2012@gmail.com>
2025-04-28 17:00:39 +0100

    Mobile navbar visibility on dark theme fix
    - Used dynamic colours to change and massively increase visibility on light/dark mode.

--- main.js
+++ main.js
@@ -185,7 +185,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	const navToggle = document.createElement("button");
 	navToggle.className = "nav-toggle";
 	navToggle.innerHTML = `
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="var(--dark-icon)" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 12H21" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
             <path d="M3 6H21" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
             <path d="M3 18H21" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
